cad2c2a28e8c3874440b06346881489df12fe980
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-06-28 14:13:18 +0200

    Html test data to test sub categories' links extraction

---
+++ sentimentscrapper/tests/test_data/html_test_data.py
@@ -0,0 +1,68 @@
+class HtmlTestData():
+    
+    sub_categories_html = ["""
+        <li>
+            <span class="a-list-item">
+                <a class="a-link-normal s-ref-text-link" href="b?ie=whatever">
+                    <span class="a-size-small a-color-base">
+                        Kids' Home Store
+                    </span>
+                </a>
+            </span>
+        </li>
+    """,
+    """
+    <html>
+        <li>
+            <span class="a-list-item">
+                <a class="a-link-normal s-ref-text-link" href="b?ie=whatever">
+                    <span class="a-color-base">
+                        Kids' Home Store
+                            <a href="b?ie=notExtracted"></a>
+                    </span>
+                </a>
+            </span>
+        </li>
+    </html>
+    """
+    ]
+
+    no_sub_categories_html = ["""
+        <li>
+            <span class="a-list-item">
+                <span></span>
+                <a class="a-link-normal s-ref-text-link" href="b?ie=notExtracted">
+                    <span class="a-size-small a-color-base">
+                        Kids' Home Store
+                    </span>
+                </a>
+            </span>
+        </li>
+    """,
+    """
+    <html>
+        <li>
+            <span class="a-list-item">
+                <a class="a-link-normal s-ref-text-link" href="/s/ref=lp_1622501101">
+                    <span class="a-size-small a-color-base">
+                        Kids' Home Store
+                    </span>
+                </a>
+            </span>
+        </li>
+    </html>
+    """,
+    """
+    <html>
+        <li>
+            <span class="a-list-item">
+                <a class="a-link-normal s-ref-text-link" href="/s/ref=lp_1622501101">
+                    <span >
+                        Kids' Home Store
+                    </span>
+                </a>
+            </span>
+        </li>
+    </html>
+    """
+    ]
