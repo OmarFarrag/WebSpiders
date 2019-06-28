7ab340f202150197ab6b7abbfcd4f7d8499eae22
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-06-28 17:35:41 +0200

    Added [test_extract_sub_categories_links]

---
+++ sentimentscrapper/tests/amazon_spider_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from sentimentscrapper.tests.test_data.html_test_data import HtmlTestData as td
+from sentimentscrapper.spiders.amazon_spider import AmazonSpider
+from scrapy.http import TextResponse
+
+class AmazonSpiderTest(unittest.TestCase):
+    
+    def setup(self):
+        pass
+
+    def test_extract_sub_categories_links(self):    
+        amazon_spider = AmazonSpider()
+        i=0
+        for data, result in td.sub_categories_html.items():
+            #TODO : unless the response is instantiated every time, it caches old data 
+            response = TextResponse(url = "",encoding="utf8")
+            response._set_body(data)
+            with self.subTest():
+                self.assertEqual(amazon_spider.extract_sub_categories_links(response), result , "data index {}".format(i))
+            i=i+1
+
+
+    if __name__ == '__main__' :   
+        unittest.main()
