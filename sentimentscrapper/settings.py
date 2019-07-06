8caae959c2f9afb485e169e20a9c5534357dbccd
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-06 20:43:02 +0200

    Pipeline name change

--- sentimentscrapper/settings.py
+++ sentimentscrapper/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'sentimentscrapper.pipelines.AmazonReviewsPipeline': 300,
+   'sentimentscrapper.pipelines.AmazonReviewsVerificationPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
