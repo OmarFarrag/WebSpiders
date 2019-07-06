f6c34c954ec5388b01202023a3d8a6e41ecd2913
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-06 18:17:03 +0200

    Review items construction from pages added

--- sentimentscrapper/pipelines.py
+++ sentimentscrapper/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class SentimentscrapperPipeline(object):
+class AmazonReviewsPipeline(object):
     def process_item(self, item, spider):
         return item
