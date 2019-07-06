e0020e7676c484c805b156bbe6c86a518d431b8f
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-07 01:29:04 +0200

    CSV item exporter added

--- sentimentscrapper/pipelines.py
+++ sentimentscrapper/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import CsvItemExporter
+from scrapy.exceptions import DropItem
 
 class AmazonReviewsVerificationPipeline(object):
     def process_item(self, item, spider):
