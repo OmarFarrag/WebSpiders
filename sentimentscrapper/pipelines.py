017c098f360ba04435dc64220e3f0e5ea0d80c28
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-07 01:28:47 +0200

    CSV item exporter added

--- sentimentscrapper/pipelines.py
+++ sentimentscrapper/pipelines.py
@@ -4,13 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.exporters import CsvItemExporter
 
 class AmazonReviewsVerificationPipeline(object):
     def process_item(self, item, spider):
         if item.get('stars'):
             if item.get('comment'):
                 if item.get('date'):
+                    print(item)
                     return item
                 else:
                     raise DropItem("Missing date")
@@ -19,3 +20,19 @@ class AmazonReviewsVerificationPipeline(object):
         else:
             raise DropItem("Missing stars")
         
+
+class AmazonReviewCSVExportPipeline(object):
+    """ Exports the extracted revirew items to a csv file """
+    
+    def open_spider(self, spider):
+        f = open('amazon_reviews.csv','wb')
+        self.exporter = CsvItemExporter(f)
+        self.exporter.start_exporting()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.exporter.file.close()
