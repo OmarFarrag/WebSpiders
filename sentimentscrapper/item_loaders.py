06718800b0cff5afdffc9b0b4bb8cf9decff5cf7
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-07 19:10:31 +0200

    Default output processor

--- sentimentscrapper/item_loaders.py
+++ sentimentscrapper/item_loaders.py
@@ -1,6 +1,6 @@
 from scrapy.loader import ItemLoader
 from sentimentscrapper.items import AmazonReviewItem
-from scrapy.loader.processors import MapCompose
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 def filter_rating(self,txts):
     for txt in txts:
@@ -12,4 +12,6 @@ class AmazonReviewLoader(ItemLoader):
         self.default_item_class = AmazonReviewItem
         return super().__init__(item=item, selector=selector, response=response, parent=parent, **context)
     
+    default_output_processor = TakeFirst()
     stars_in = filter_rating
+
