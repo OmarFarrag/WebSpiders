12626d6f9e97874fbcd508a00d225f492b6b67b8
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-07-09 02:22:05 +0200

    Added keywords processors

--- sentimentscrapper/item_loaders.py
+++ sentimentscrapper/item_loaders.py
@@ -1,10 +1,14 @@
 from scrapy.loader import ItemLoader
 from sentimentscrapper.items import AmazonReviewItem
-from scrapy.loader.processors import MapCompose, TakeFirst
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
 
 def filter_rating(self,txts):
     for txt in txts:
         yield float(txt.split()[0])*0.2
+
+def filter_keywords(self, txts):
+    for txt in txts:
+        yield txt.strip('\n ')
 
 
 class AmazonReviewLoader(ItemLoader):
@@ -13,5 +17,9 @@ class AmazonReviewLoader(ItemLoader):
         return super().__init__(item=item, selector=selector, response=response, parent=parent, **context)
     
     default_output_processor = TakeFirst()
+    
     stars_in = filter_rating
+
+    keywords_in = filter_keywords
+    keywords_out = Join('-')
 
